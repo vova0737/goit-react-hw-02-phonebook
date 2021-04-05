c9f023b094dddc093053d1fe1b54713189c78f2d
Volodymyr Bondar <vova0737@gmail.com>
2021-04-05 23:27:48 +0300

    components init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
+import React, { Component } from 'react';
+import Container from './Components/Container';
+import Section from './Components/Section';
+import ContactForm from './Components/ContactForm';
+import ContactList from './Components/ContactList';
+import Filter from './Components/Filter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '+380 93 555 77 88' },
+  { id: 'id-2', name: 'Hermione Kline', number: '+380 67 442 74 98' },
+  { id: 'id-3', name: 'Eden Clements', number: '+380 63 005 71 84' },
+  { id: 'id-4', name: 'Annie Copeland', number: '+380 79 815 07 84' },
+];
+
+class App extends Component {
+  state = {
+    contacts: initialState,
+    filter: '',
+  };
+
+
+
+};
 
 export default App;
